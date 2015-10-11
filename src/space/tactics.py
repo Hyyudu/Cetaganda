78c80efa130cd2cb01f06660821d4771280d1845
Mikhail Polykovskij <glader@yandex-team.ru>
2015-10-11 11:39:36 +0500

    Catch transports

--- src/space/tactics.py
+++ src/space/tactics.py
@@ -64,6 +64,22 @@ def fight(fleet):
 
         alive = fight_with_enemy(fleet, enemy_fleet)
         if not alive:
+            # Транспорты меняют принадлежность
+            if fleet.ship_set.filter(is_alive=True).exists():
+                for ship in fleet.ship_set.filter(is_alive=True):
+                    log.info('Ship "%s" stolen by %s' % (ship, enemy_fleet.navigator))
+                    ship.owner.records.create(
+                        category='Космос',
+                        message='Вы утратили корабль "%s"' % ship,
+                    )
+                    enemy_fleet.navigator.records.create(
+                        category='Космос',
+                        message='Вы захватили корабль "%s"' % ship,
+                    )
+
+                    ship.fleet = enemy_fleet
+                    ship.owner = enemy_fleet.navigator
+                    ship.save()
             return False
 
     return True
@@ -77,6 +93,11 @@ def fight_with_enemy(fleet, enemy_fleet):
             return
 
         if ship.friends.filter(id=target.id).exists() and target.friends.filter(id=ship.id).exists():
+            log.info('"%s" and "%s" are friends' % (ship, target))
+            return
+
+        if models.Alliance.get_alliance(ship.owner) == models.Alliance.get_alliance(target.owner):
+            log.info('"%s" and "%s" from the same alliance' % (ship, target))
             return
 
         log.info("HIT from %s TO %s", ship, target)
@@ -98,24 +119,25 @@ def fight_with_enemy(fleet, enemy_fleet):
         targets = False
 
         our_shots = []
-        our_side = fleet.ship_set.all()
+        our_side = fleet.ship_set.filter(in_space=True, is_alive=True).exclude(type='t')
         our_side = sorted(our_side, key=lambda ship: models.SHIPS[ship.type]['hit_priority'])
+        log.info('OUR_SIDE %s', our_side)
         if not our_side:
             return False
 
         their_shots = []
-        their_side = enemy_fleet.ship_set.all()
+        their_side = enemy_fleet.ship_set.filter(in_space=True, is_alive=True).exclude(type='t')
         their_side = sorted(their_side, key=lambda ship: models.SHIPS[ship.type]['hit_priority'])
+        log.info('THEIR_SIDE %s', their_side)
         if not their_side:
             return True
 
         for ship in our_side:
-            dice = str(random.randint(0, 9))
-            log.info('DICE %s', dice)
-            if dice in models.SHIPS[ship.type]['hit']:
-                log.info('SHOT %s', ship)
+            if _is_shoot(ship):
+                log.info('OUR SHOT %s', ship)
                 our_shots.append(ship)
             else:
+                log.info('OUR MISS %s', ship)
                 ship.owner.records.create(
                     category='Космос',
                     message='Ваш корабль "%s" промахивается' % ship
@@ -124,12 +146,11 @@ def fight_with_enemy(fleet, enemy_fleet):
         log.info('OUR_SHOTS %s', our_shots)
 
         for ship in their_side:
-            dice = str(random.randint(0, 9))
-            log.info('DICE %s', dice)
-            if dice in models.SHIPS[ship.type]['hit']:
+            if _is_shoot(ship):
                 log.info('ENEMY SHOT %s', ship)
                 their_shots.append(ship)
             else:
+                log.info('ENEMY MISS %s', ship)
                 ship.owner.records.create(
                     category='Космос',
                     message='Ваш корабль "%s" промахивается' % ship
@@ -156,7 +177,16 @@ def fight_with_enemy(fleet, enemy_fleet):
         if not targets:
             break
 
-    return fleet.ship_set.all().exists()
+    return fleet.ship_set.filter(in_space=True).exclude(type='t').exists()
+
+
+def _is_shoot(ship):
+    dice = str(random.randint(0, 9))
+    log.info('DICE %s', dice)
+    if dice in models.SHIPS[ship.type]['hit']:
+        log.info('ENEMY SHOT %s', ship)
+        return True
+    return False
 
 
 def harvest(fleet):
--- src/tests/test_fight.py
+++ src/tests/test_fight.py
@@ -5,11 +5,12 @@ import random
 import pytest
 from mock import patch
 
+from register.models import Record
+from space import tactics
 from space.models import Ship, Fleet, Report
 from space.tactics import move_fleets
-from register.models import Record
-from tests.utils import refresh
 from tests.test_space import create_ship
+from tests.utils import refresh
 
 pytestmark = pytest.mark.django_db
 
@@ -57,3 +58,111 @@ def test_fight(roles, points, alliances):
         '''Корабль "frodoship" (Переход 1 (t)) переместился в "Переход 2 (t)"
 Корабль "legoship" (Переход 2 (t)) уничтожен
 '''
+
+
+def test_enemy_scout(roles, points, alliances):
+    ship1, fleet1 = create_ship('k', roles['frodo'], roles['boromir'], points[3], name='frodoship')
+    ship2, fleet2 = create_ship('r', roles['legolas'], roles['aragorn'], points[4], name='legoship')
+
+    fleet1.route = str(points[4].id)
+    fleet1.save()
+
+    with patch.object(tactics, '_is_shoot') as shoot:
+        shoot.return_value = 1
+
+        move_fleets()
+
+    ship1 = refresh(ship1)
+    assert ship1.is_alive
+
+    ship2 = Ship.all.get(pk=ship2.id)
+    assert ship2.is_alive
+
+
+def test_scout(roles, points, alliances):
+    ship1, fleet1 = create_ship('r', roles['frodo'], roles['boromir'], points[3], name='frodoship')
+    ship2, fleet2 = create_ship('k', roles['legolas'], roles['aragorn'], points[4], name='legoship')
+
+    fleet1.route = str(points[4].id)
+    fleet1.save()
+
+    with patch.object(tactics, '_is_shoot') as shoot:
+        shoot.return_value = 1
+
+        move_fleets()
+
+    ship1 = refresh(ship1)
+    assert ship1.is_alive
+
+    ship2 = Ship.all.get(pk=ship2.id)
+    assert ship2.is_alive
+
+
+def test_one_navigator(roles, points, alliances):
+    ship1, fleet1 = create_ship('k', roles['frodo'], roles['boromir'], points[3], name='frodoship')
+    ship2, fleet2 = create_ship('k', roles['legolas'], roles['boromir'], points[4], name='legoship')
+
+    fleet1.route = str(points[4].id)
+    fleet1.save()
+
+    with patch.object(tactics, '_is_shoot') as shoot:
+        shoot.return_value = 1
+
+        move_fleets()
+
+    ship1 = refresh(ship1)
+    assert ship1.is_alive
+
+    ship2 = refresh(ship1)
+    assert ship2.is_alive
+
+
+def test_one_alliance(roles, points, alliances):
+    ship1, fleet1 = create_ship('k', roles['frodo'], roles['frodo'], points[3], name='frodoship')
+    ship2, fleet2 = create_ship('k', roles['aragorn'], roles['legolas'], points[4], name='aragornship')
+
+    fleet1.route = str(points[4].id)
+    fleet1.save()
+
+    with patch.object(tactics, '_is_shoot') as shoot:
+        shoot.return_value = 1
+
+        move_fleets()
+
+    ship1 = refresh(ship1)
+    assert ship1.is_alive
+
+    ship2 = Ship.all.get(pk=ship2.id)
+    assert ship2.is_alive
+
+
+def test_catch_transport(roles, points, alliances):
+    ship1, fleet1 = create_ship('k', roles['frodo'], roles['frodo'], points[3], name='frodoship')
+    ship2, fleet1 = create_ship('t', roles['frodo'], roles['frodo'], points[3], name='frodotransport', fleet=fleet1)
+    ship3, fleet2 = create_ship('k', roles['legolas'], roles['legolas'], points[4], name='legolasship')
+
+    fleet1.route = str(points[4].id)
+    fleet1.save()
+
+    def dice():
+        yield 5  # our cruiser miss
+        yield 1  # their cruiser hit
+
+    with patch.object(random, 'randint') as randint:
+        randint.side_effect = dice()
+
+        move_fleets()
+
+    ship1 = Ship.all.get(pk=ship1.id)
+    assert not ship1.is_alive
+    assert ship1.owner == roles['frodo']
+
+    ship2 = refresh(ship2)
+    assert ship2.is_alive
+    assert ship2.owner == roles['legolas']
+
+    ship3 = refresh(ship3)
+    assert ship3.is_alive
+
+    assert roles['frodo'].records.order_by('-dt')[0].message == 'Вы утратили корабль "%s"' % ship2
+    assert roles['legolas'].records.order_by('-dt')[0].message == 'Вы захватили корабль "%s"' % ship2
